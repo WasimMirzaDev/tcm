de086f426b2056c176cda515b9156f16eea27227
WasimMirzaDev <falconflyyer123@gmail.com>
2022-12-24 16:20:50 +0500

    Update PrintFulController.php

--- app/Http/Controllers/PrintFulController.php
+++ app/Http/Controllers/PrintFulController.php
@@ -21,7 +21,7 @@ class PrintFulController extends Controller
   // dd($pf);
   // create Products Api object
   $productsApi = new PrintfulProducts($pf);
-  // dd($productsApi);
+  dd($productsApi);
   // set some paging info
   $offset = 0;
   $limit = 20;
